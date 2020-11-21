dbd721e0c6022832baf48fd67ab51c9155079fcc
YitianJiang <2138127826@qq.com>
2020-11-21 22:26:55 +0800

    add sedmutipleline

---
+++ sedmutipleline/sedmutipleline.sh
@@ -0,0 +1,10 @@
+sed -i '/<configuration>/a \
+<property>\
+        <name>hadoop.tmp.dir</name>\
+        <value>/root/hadoop/tmp</value>\
+        <description>Abase for other temporary directories.</description>\
+</property>\
+<property>\
+        <name>fs.default.name</name>\
+        <value>hdfs://hserver1:9000</value>\
+</property>' test1
