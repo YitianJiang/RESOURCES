50ccc271358f35c3c16086631239b4ac0ada3a38
YitianJiang <2138127826@qq.com>
2020-12-19 21:46:08 +0800

    shell脚本粘贴出现的command not found

---
+++ shell粘贴的代码不能运行但看上去没有错误/changeiptest修改后.sh
@@ -0,0 +1,12 @@
+findfiles(){
+for file in $(ls $1)
+  do
+    if [ -d $1"/"$file ] ;then
+      findfiles $1"/"$file
+    else
+      echo ${file}
+    fi
+  done
+}
+findfiles /root/system-install-and-config
+
